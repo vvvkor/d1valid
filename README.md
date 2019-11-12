0266920401c185c6c35545f6a67851b5228aedc1
vvvkor <vadimkor@yandex.ru>
2019-11-12 17:23:34 +0300

    live validation (optional)

--- README.md
+++ README.md
@@ -19,6 +19,11 @@ d1valid.init(options);
 
 ## Options
 
+### cLiveVal
+
+CSS class of forms, for which inputs are validated on the fly and submit buttons are disabled if form is in invalid state.  
+Default: ``"js-live-val"``
+
 ### cUnhint
 
 CSS class of forms, for which default brower-specific tooltips are disabled and empty required inputs are not reported as invalid until submitting.  
--- d1valid.js
+++ d1valid.js
@@ -2,7 +2,6 @@
 /* Custom form validation */
 
 /* todo: ajax validate, server validate, .js-process */
-/* todo: required lookup, wysiwyg, min/max date */
 
 //form, form.js-validate, input[data-hint]~.small
 if(typeof module !== "undefined") var d1 = require('d1css');
@@ -15,24 +14,9 @@ var main = new(function () {
   
   this.opt = {
     qsValidate: 'form', // set custom text for browser tooltips
-    cUnhint: 'js-unhint' // turn off browser tooltips
+    cUnhint: 'js-unhint', // turn off browser tooltips
+    cLiveVal: 'js-live-val' // live validation, disable submit buttons if invalid
   };
-  
-  /*
-  this.validationErrors = [
-    'valueMissing',
-    'typeMismatch',
-    'tooLong',
-    'tooShort',
-    'patternMismatch',
-    'rangeUnderflow',
-    'rangeOverflow',
-    'stepMismatch',
-    'badInput',
-    'customError'
-    //,'valid'
-  ];
-  */
   
   this.init = function (opt) {
     var i;
@@ -40,6 +24,7 @@ var main = new(function () {
     var q = this.opt.qsValidate;
     d1.b('', q + " input, " + q + " textarea, "+ q +" select", "", this.initInput.bind(this));
     d1.b('', "form."+this.opt.cUnhint, "", this.unhint.bind(this));
+    d1.b('', "form."+this.opt.cLiveVal, "", this.validateForm.bind(this));
     d1.b('', "form."+this.opt.cUnhint, "submit", this.validateForm.bind(this));
   }
   
@@ -51,27 +36,21 @@ var main = new(function () {
     }
   }
 
+  this.isLive = function(f){
+    return (f && f.classList.contains(this.opt.cLiveVal));
+  }
+  
   this.validateInput = function(n) {
     if (n.type == 'radio') d1.b(n.form, '[name="'+n.name+'"]', '', function(m){ m.setCustomValidity(''); });
     else n.setCustomValidity('');
     n.checkValidity();
+    if(this.isLive(n.form)) this.validateForm(n.form);
   }
 
   this.setCustomMessage = function(n) {
     var t = n.getAttribute('data-hint') || '';// || n.title;
     t = t.replace(/%([\w\-]+)%/g, function(m,v){ return n.getAttribute(v); })
     n.setCustomValidity(t);
-    /*
-    var x = '', err = '', i = 0;
-    while (!x && (err=this.validationErrors[i++])){
-      if(n.validity[err]) x = d1.s(err + ('_' + (n.type || n.tagName.toLowerCase() || '')), '') || d1.s(err,'');
-    }
-    if (x) {
-      x = x.replace(/%(\w+)%/g, function(m,v){ return n.getAttribute(v); });
-      if (n.title.length > 0) x += " \n" + n.title;
-    }
-    n.setCustomValidity(x);
-    */
   }
   
   this.unhint = function(n, e) {
@@ -79,13 +58,16 @@ var main = new(function () {
   }
   
   this.validateForm = function(n, e) {
-    n.classList.remove(this.opt.cUnhint);
-    if (n.checkValidity() === false) {
+    if(e) n.classList.remove(this.opt.cUnhint);
+    var ok = n.checkValidity();//!==false
+    if (!ok && e) {
       e.preventDefault();
       e.stopPropagation();
       var f = d1.q(':invalid', 0, n);
       if(f) f.focus();
     }
+    //d1.b(n, '[type="submit"]', '', function(m){ m.disabled = !ok; });//if no cUnhint
+    if(this.isLive(n)) d1.b(n, '[type="submit"]', '', function(m){ m.classList[ok ? 'remove' : 'add']('bg-n'); });//if cUnhint used
   }
   
   d1.plug(this);
